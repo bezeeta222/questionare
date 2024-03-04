e413749beefd79e0a1bd583153ed5eafc53e69da
bezeeta222 <azadam8255@gmail.com>
2024-03-04 15:32:30 +0800

    add landing page

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatar.vercel.sh'
-      }
-    ]
-  }
-};
+// ----------------------------------------------------------------------
 
-module.exports = nextConfig;
+module.exports = {
+  trailingSlash: true,
+};
